f86c9a702a2eca39c22c74b5c7f5d0bc9bc822d4
akn <akn@AKNs-MacBook-Pro.local>
2022-04-04 13:46:31 +0630

    [create] isemail validator

--- index.js
+++ index.js
@@ -33,6 +33,26 @@ class Validator{
         return this;
     }
 
+    isEmail= (_msg) => {
+        const validateEmail = (email) => {
+            return String(email)
+              .toLowerCase()
+              .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+        };    
+    
+        if(this._input === undefined){
+            this._inputErr.push(_msg);
+            this.errors[this._input] = this._inputErr;
+        }
+        else if(!validateEmail(this._input)){
+            this._inputErr.push(_msg);
+            this.errors[this._input] = this._inputErr;
+        }
+        return this;
+    }
+
     isNumber=(_msg)=>{
         if(this._input === undefined){
             this._inputErr.push(_msg);
